12e837979df9b6820c57c0580957ea7700fe2706
Jojo <jonathansardeamusa@gmail.com>
2022-07-06 02:00:59 +0800

    fixed uppercase letters turning to lowercase

--- cipher.rb
+++ cipher.rb
@@ -1,12 +1,17 @@
 require "pry-byebug"
+
 def caesar_cipher(string,shift)
+  shift %= 26 #make shift not greater than 26
   array = string.split("")
-  char_array = array.map do |char| 
+  char_array = array.map do |char|
     if char.match(/[a-zA-Z]/)
       number = char.ord + shift
-      number -= 26 if (number > 90 && number < 97 || number > 122)
+      number -= 26 if (/[A-Z]/.match(char) && number >= 90 || number > 122)
       char = number.chr
     else char end
   end
-  puts char_array.join("")
+  new_string = char_array.join("")
+  p new_string
 end
+
+caesar_cipher("Zehahahahaha!!! zzzz and punctuation....!!!!????? SaRcASm SpeAK", 45)
